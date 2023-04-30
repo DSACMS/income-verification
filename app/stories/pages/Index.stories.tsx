0f8614ad7991a7c6a170d1d996a109db7b56a58a
Sawyer <git@sawyerh.com>
2023-04-30 09:58:12 -0700

    Direct ink

--- app/stories/pages/Index.stories.tsx
+++ app/stories/pages/Index.stories.tsx
@@ -1,7 +1,4 @@
-import { linkTo } from "@storybook/addon-links";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-
-
 
 import Index from "../../src/pages/index";
 
@@ -9,8 +6,11 @@ export default {
   title: "UploadPage",
 } as ComponentMeta<typeof Index>;
 
-const Template: ComponentStory<typeof Index> = () => (
-  <Index onSubmit={linkTo("Confirmation Page")} />
-);
+const test = () => {
+  window.location.href =
+    "/iframe.html?args=&id=confirmation-page--confirmation-page&viewMode=story";
+};
+
+const Template: ComponentStory<typeof Index> = () => <Index onSubmit={test} />;
 
 export const UploadPage = Template.bind({});
