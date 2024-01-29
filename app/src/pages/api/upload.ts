484ffaec90e0b5a0198874884e41d8dacced21af
Matt Gardner <wilburnforce@gmail.com>
2024-01-29 14:10:59 -0500

    Accept file upload and post to a next API

---
+++ app/src/pages/api/upload.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export const config = {
+  api: {
+      bodyParser: {
+          sizeLimit: '10mb' // Set desired value here
+      }
+  }
+}
+
+type ResponseData = {
+  message: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  if (req.method === 'POST') {
+    res.status(200).json({ message: 'Success!' });
+  }
+}
--- app/src/pages/upload/index.tsx
+++ app/src/pages/upload/index.tsx
@@ -2,11 +2,11 @@ import {
   Accordion,
   Button,
   FileInput,
-  FormGroup,
+  Form,
   Label,
 } from "@trussworks/react-uswds";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, SyntheticEvent } from "react";
 import Layout from "src/components/Layout";
 
 const Home = (props: { onSubmit?: () => void }) => {
@@ -22,13 +22,40 @@ const Home = (props: { onSubmit?: () => void }) => {
     }
   }, []);
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onFileChange = async (/* e: ChangeEvent */) => {
+    // check bluriness as part of the preview process?
+  }
+
+  const onSubmit = async (e: SyntheticEvent): Promise<void> => {
+    e.preventDefault();
+
     if (props.onSubmit) {
       props.onSubmit();
       return;
     }
 
-    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      fileInputMultiple: { files: string };
+    };
+    const fileInput = target.fileInputMultiple;
+
+    if (fileInput) {
+      const [file] = fileInput.files;
+      const body = new FormData();
+      body.append("file", file);
+
+      try {
+        await fetch("/api/upload/", {
+          method: "POST",
+          body
+        });
+      } catch (e) {
+        console.log(e);
+      } finally {
+        console.log('finished');
+      }
+    }
+
     router.push("/upload/confirmation").catch(console.error);
   };
 
@@ -44,85 +71,84 @@ const Home = (props: { onSubmit?: () => void }) => {
           this secure site.
         </p>
 
-        <form className="usa-form usa-form--large" onSubmit={onSubmit}>
-          <FormGroup>
-            <Label
-              htmlFor="file-input-multiple"
-              className="text-bold mobile-lg:font-body-lg"
-            >
-              Select files or photos
-            </Label>
-            <span className="usa-hint" id="file-input-multiple-hint">
-              Files should be in PDF, JPG, PNG, TIFF, or HEIC format. Files must
-              be under 10MB.
-            </span>
-            <FileInput
-              crossOrigin={true}
-              id="file-input-multiple"
-              name="file-input-multiple"
-              aria-describedby="file-input-multiple-hint"
-              multiple
-            />
+        <Form className="usa-form usa-form--large" onSubmit={onSubmit}>
+          <Label
+            htmlFor="fileInputMultiple"
+            className="text-bold mobile-lg:font-body-lg"
+          >
+            Select files or photos
+          </Label>
+          <span className="usa-hint" id="file-input-multiple-hint">
+            Files should be in PDF, JPG, PNG, TIFF, or HEIC format. Files must
+            be under 10MB.
+          </span>
+          <FileInput
+            crossOrigin="true"
+            id="fileInputMultiple"
+            name="fileInputMultiple"
+            aria-describedby="file-input-multiple-hint"
+            onChange={onFileChange}
+            multiple
+          />
 
-            <Accordion
-              className="maxw-tablet margin-y-2"
-              bordered
-              items={[
-                {
-                  title: "Tips for scanning and taking photos",
-                  content: (
-                    <>
-                      <p>
-                        You can upload digital files, scans, or photos of
-                        documents from a phone or tablet. Make sure your
-                        documents are easy to read and include your name or
-                        business name.
-                      </p>
+          <Accordion
+            className="maxw-tablet margin-y-2"
+            bordered
+            items={[
+              {
+                title: "Tips for scanning and taking photos",
+                content: (
+                  <>
+                    <p>
+                      You can upload digital files, scans, or photos of
+                      documents from a phone or tablet. Make sure your
+                      documents are easy to read and include your name or
+                      business name.
+                    </p>
 
-                      <p>
-                        If you have an Apple or Android device, we recommend
-                        scanning documents with built-in apps:
-                      </p>
+                    <p>
+                      If you have an Apple or Android device, we recommend
+                      scanning documents with built-in apps:
+                    </p>
 
-                      <ul className="usa-list">
-                        <li>
-                          <a
-                            className="usa-link usa-link--external"
-                            target="_blank"
-                            rel="noreferrer"
-                            href="https://support.apple.com/en-us/HT210336"
-                          >
-                            Scan documents with an iPhone or iPad
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            className="usa-link usa-link--external"
-                            target="_blank"
-                            rel="noreferrer"
-                            href="https://support.google.com/drive/answer/3145835?hl=en&co=GENIE.Platform%3DAndroid"
-                          >
-                            Scan documents with Google Drive
-                          </a>
-                        </li>
-                      </ul>
-                      <p>
-                        Use a solid, dark background when scanning or taking
-                        photos. Do not use a flash or alter the images in any
-                        way.
-                      </p>
-                    </>
-                  ),
-                  expanded: false,
-                  id: "accordion_2",
-                  headingLevel: "h2",
-                },
-              ]}
-            />
-          </FormGroup>
-
+                    <ul className="usa-list">
+                      <li>
+                        <a
+                          className="usa-link usa-link--external"
+                          target="_blank"
+                          rel="noreferrer"
+                          href="https://support.apple.com/en-us/HT210336"
+                        >
+                          Scan documents with an iPhone or iPad
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          className="usa-link usa-link--external"
+                          target="_blank"
+                          rel="noreferrer"
+                          href="https://support.google.com/drive/answer/3145835?hl=en&co=GENIE.Platform%3DAndroid"
+                        >
+                          Scan documents with Google Drive
+                        </a>
+                      </li>
+                    </ul>
+                    <p>
+                      Use a solid, dark background when scanning or taking
+                      photos. Do not use a flash or alter the images in any
+                      way.
+                    </p>
+                  </>
+                ),
+                expanded: false,
+                id: "accordion_2",
+                headingLevel: "h2",
+              },
+            ]}
+          />
           <Button type="submit">Submit documents</Button>
-        </form>
+        </Form>
+
       </div>
     </Layout>
   );
