a3928ec1568def64eab63201a4a94a067aaa0c58
Matt Gardner <wilburnforce@gmail.com>
2024-02-23 15:22:57 -0500

    pray

--- app/src/utils/blur-detector.ts
+++ app/src/utils/blur-detector.ts
@@ -1,10 +1,19 @@
 import sharp from "sharp";
+import photon from "@silvia-odwyer/photon-node"
+import fs from "fs";
 
-export type BlurryDetectorReport = {
-  imagePath: string;
-  isBlurry: boolean;
-  score: number;
-};
+function computeVariance(vector: Buffer | Uint8Array) {
+  const mean =
+    vector.reduce((sum, value) => sum + value, 0) /
+    vector.length;
+  const variance =
+    vector.reduce(
+      (sum, value) => sum + Math.pow(value - mean, 2),
+      0
+    ) / vector.length;
+
+  return variance;
+}
 
 // https://github.com/puntorigen/blurry-detector/blob/main/index.js
 class BlurryDetector {
@@ -22,30 +31,33 @@ class BlurryDetector {
       kernel: [0, 1, 0, 1, -4, 1, 0, 1, 0],
     };
 
+    const base64 = fs.readFileSync(imagePath, { encoding: 'base64' });
+    const data = base64.replace(/^data:image\/(png|jpg);base64,/, "");
+    const phtn_img = photon.PhotonImage.new_from_base64(data);
+    photon.grayscale(phtn_img);
+    photon.laplace(phtn_img);
+    const output = phtn_img.get_base64();
+    const laplacianImageData = photon.base64_to_vec(output.replace(/^data:image\/(png|jpg);base64,/, ""));
+
     const jpegified = await sharp(imagePath).toFormat("jpeg").toBuffer();
 
     // Convolve the image with the Laplacian kernel
-    const laplacianImageData = await sharp(jpegified)
+    const laplacianImageData2 = await sharp(jpegified)
       .greyscale()
-      .resize({ width: 1000, withoutReduction: true })
       .raw()
       .convolve(laplacianKernel)
       .toBuffer();
 
+    console.log(computeVariance(laplacianImageData), computeVariance(laplacianImageData2));
     // Compute the variance of the Laplacian image
-    const mean =
-      laplacianImageData.reduce((sum, value) => sum + value, 0) /
-      laplacianImageData.length;
-    const variance =
-      laplacianImageData.reduce(
-        (sum, value) => sum + Math.pow(value - mean, 2),
-        0
-      ) / laplacianImageData.length;
+
+    const variance = computeVariance(laplacianImageData);
+
     return variance;
   }
 
-  async analyse(imagePath: string): Promise<BlurryDetectorReport> {
-    let variance = -1;
+  async analyse(imagePath: string): Promise<{ isBlurry: boolean, score: number }> {
+    let variance;
     try {
       variance = await this.computeLaplacianVariance(imagePath);
     } catch (e) {
@@ -53,7 +65,6 @@ class BlurryDetector {
     }
 
     return {
-      imagePath,
       isBlurry: variance < this.threshold,
       score: variance,
     };
