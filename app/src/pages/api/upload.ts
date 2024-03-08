b47c456caac5b9d22862f69de9999fab12729a4a
George Byers <github@georgebyers.me>
2024-03-08 08:39:09 -0500

    installed node-mocks-http, setup test suite for api routes

--- app/src/pages/api/upload.ts
+++ app/src/pages/api/upload.ts
@@ -1,17 +1,12 @@
-import formidable from "formidable";
-import fs from "fs";
 import type { NextApiRequest, NextApiResponse } from "next";
+import type { BlurryDetectorReport } from "../../utils/blur-detector";
+import OCR, { type ProcessedRotatedImagesResult } from "@/service/ocr";
+import { createDocumentImage } from "@/service/factories";
+import BlurryDetector from "../../utils/blur-detector";
+import formidable from "formidable";
 import os from "os";
 import path from "path";
-
-import BlurryDetector from "../../utils/blur-detector";
-import type { BlurryDetectorReport } from "../../utils/blur-detector";
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+import fs from "fs";
 
 type BlurryDetectorResults = Array<{
   status: "fulfilled" | "rejected";
@@ -19,16 +14,71 @@ type BlurryDetectorResults = Array<{
   reason?: BlurryDetectorReport;
 }>;
 
+type Files = formidable.Files<string>;
+
 export type ResponseData = {
   message: string;
   results?: BlurryDetectorResults;
 };
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const {processDocument} = OCR;
 
 // The threshold is based on a few articles about this approach
 // "The threshold can be set based on performance on the data we have."
 // Further testing is needed to better attune it to doc upload
 // https://www.linkedin.com/pulse/pinpointing-blurry-images-simple-nodejs-way-pablo-schaffner-bofill/
 const slightBlurDetector = new BlurryDetector(300);
+
+const ocrDetectionAction = async (files: Files): Promise<ProcessedRotatedImagesResult[]> => {
+  const results = await Promise.allSettled(
+    Object.values(files).map(async (filelist = []) => {
+      const [file] = filelist;
+      if (file) {
+        const saveTo = path.join(os.tmpdir(), file.originalFilename || "");
+        fs.writeFileSync(saveTo, fs.readFileSync(file.filepath));
+        const document = await createDocumentImage(saveTo);
+        const processed = await processDocument(document);
+        return processed;
+      }
+    })
+  );
+
+  return results;
+}
+
+const blurDectionAction = async (files: Files) => {
+  const results: BlurryDetectorResults = await Promise.allSettled(
+     Object.values(files).map(async (filelist = []) => {
+      const [file] = filelist;
+      if (file) {
+        const saveTo = path.join(os.tmpdir(), file.originalFilename || "");
+        fs.writeFileSync(saveTo, fs.readFileSync(file.filepath));
+        const result = await slightBlurDetector.analyse(saveTo);
+
+        console.log(
+          `File [${saveTo}] is blurry? ${
+            result.isBlurry ? "yes" : "no"
+          } with ${result.score}`
+        );
+
+        if (result.isBlurry) {
+          // rejects the promise
+          throw result;
+        }
+
+        return result;
+      }
+    })
+  );
+
+  return results;
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -37,35 +87,23 @@ export default async function handler(
   if (req.method === "POST") {
     const form = formidable({});
     const [, files] = await form.parse(req);
+    // get the processingType from the query string
+    const { processingType } = req.query;
+    // processing type can be ocr or blur
+    if (processingType === "ocr") {
+      const results = await ocrDetectionAction(files);
+      res.status(200).json({
+        message: "Success!",
+        results,
+      });
+    }
 
-    const results: BlurryDetectorResults = await Promise.allSettled(
-      Object.values(files).map(async (filelist = []) => {
-        const [file] = filelist;
-        if (file) {
-          const saveTo = path.join(os.tmpdir(), file.originalFilename || "");
-          fs.writeFileSync(saveTo, fs.readFileSync(file.filepath));
-          const result = await slightBlurDetector.analyse(saveTo);
-
-          console.log(
-            `File [${saveTo}] is blurry? ${
-              result.isBlurry ? "yes" : "no"
-            } with ${result.score}`
-          );
-
-          if (result.isBlurry) {
-            // rejects the promise
-            throw result;
-          }
-
-          return result;
-        }
-      })
-    );
-
-    // todo: handle failure scenario!
-    res.status(200).json({
-      message: "Success!",
-      results,
-    });
+    if(processingType === "blur") {
+      const results = await blurDectionAction(files);
+      res.status(200).json({
+        message: "Success!",
+        results,
+      });
+    }
   }
 }
---
+++ app/tests/api/upload/handler.spec.ts
@@ -0,0 +1,82 @@
+import { describe, it, expect, vi, beforeAll } from 'vitest';
+import handler from '@/pages/api/upload'; // Adjust the import path as necessary
+import { createMocks } from 'node-mocks-http';
+import { createDocumentImage } from '@/service/factories';
+import { type DocumentImage} from '@/utils/document';
+import path from 'path';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+let testDocument: DocumentImage;
+
+// Mock `formidable` correctly as a function
+vi.mock('formidable', () => {
+  return {
+    // Mock the default export to be a function that returns an instance with a `parse` method
+    default: () => ({
+      parse: () => {
+        // Simulate the file structure that formidable would produce
+        const file1 = { filepath: testDocument.sourcePath, originalFilename: testDocument.fileName, mimetype: 'image/jpeg' };
+        const file2 = { filepath: '/tmp/uploaded2.png', originalFilename: 'test2.png', mimetype: 'image/png' };
+        // Simulated files object
+        const files = { 'file1': [file1], 'file2': [file2] };
+        // Simulated fields object (empty in this case)
+        const fields = {};
+       return [fields, files];
+      },
+    }),
+  };
+});
+
+describe('File Upload API Endpoint', () => {
+  beforeAll(async () => {
+    vi.clearAllMocks();
+    // setup our test document
+    const testDocumentPath = path.join(
+        __dirname,
+        "../../fixture/adp-earnings-statement1.jpeg"
+      );
+    testDocument = await createDocumentImage(testDocumentPath);
+  });
+
+  it('should handle multiple file uploads', async () => {
+    const { req, res } = createMocks({
+        method: 'POST',
+        query: { processingType: 'blur' },
+    });
+
+    // Do some type casting to make TS happy since we don't mock the entire req and res objects
+    const nextReq = req as unknown as NextApiRequest;
+    const nextRes = res as unknown as NextApiResponse;
+
+    await handler(nextReq, nextRes);
+    const result = res._getJSONData() as unknown;
+    const expected = {
+        message: "Success!",
+        results: [
+          {
+            status: "fulfilled",
+            value: {
+              imagePath: "/var/folders/g4/chfpyqbn7l74n7v9__5y4pwh0000gp/T/adp-earnings-statement1",
+              isBlurry: false,
+              score: 1125.6740445003702,
+            },
+          },
+          {
+            status: "rejected",
+            reason: {
+              errno: -2,
+              code: "ENOENT",
+              syscall: "open",
+              path: "/tmp/uploaded2.png", // this file definitely doesn't exist
+            },
+          },
+        ],
+      };
+
+    // Check that the response status code is 200
+    expect(result).toEqual(expected);
+    expect(res._getStatusCode()).toBe(200);
+    expect(res._getJSONData()).toHaveProperty('message', 'Success!');
+  });
+
+});
